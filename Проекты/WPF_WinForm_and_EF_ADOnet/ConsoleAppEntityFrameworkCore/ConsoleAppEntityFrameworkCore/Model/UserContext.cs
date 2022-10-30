19198f234dfd98590a0f8c6f4d45dd3d210414b6
Stellrays <91783144+stellrays@users.noreply.github.com>
2022-10-30 14:35:01 +0300

    Add files via upload

---
+++ Проекты/WPF_WinForm_and_EF_ADOnet/ConsoleAppEntityFrameworkCore/ConsoleAppEntityFrameworkCore/Model/UserContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.IdentityModel.Protocols;
+namespace ConsoleAppEntityFrameworkCore.Model
+{
+    public partial class UserContext : DbContext
+    {
+        public UserContext()
+        {
+        }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users => Set<User>();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server = DESKTOP-0VRO2QB\SQLEXPRESS_2;Database=UserDatabase;Trusted_Connection=True;");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
+
---
+++ Проекты/WPF_WinForm_and_EF_ADOnet/ConsoleAppEntityFrameworkCore/ConsoleAppEntityFrameworkCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.PortableExecutable;
+
+using ConsoleAppEntityFrameworkCore.Model;
+
+using static ConsoleAppEntityFrameworkCore.Model.User;
+using (UserContext db = new UserContext())
+{
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Вывод данных:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"ID\t Name\t Age\n" +
+            $"{u.Id}\t {u.Name}\t {u.Age}");
+    }
+}
