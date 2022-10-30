58d18a1a03ca0969a9fc6b41f5241fddd6623a43
Stellrays <91783144+stellrays@users.noreply.github.com>
2022-10-30 19:11:48 +0300

    Add files via upload

---
+++ Проекты/WPF_WinForm_and_EF_ADOnet/WPFEntityFrameworkCore/WPFEntityFrameworkCore/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using WPFEntityFrameworkCore.Model;
+namespace WPFEntityFrameworkCore
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UserContext db;
+            db = new UserContext();
+            db.Users.Load();
+            UsersGrid.ItemsSource = db.Users.Local.ToBindingList();
+        }
+    }
+}
---
+++ Проекты/WPF_WinForm_and_EF_ADOnet/WPFEntityFrameworkCore/WPFEntityFrameworkCore/Model/UserContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+namespace WPFEntityFrameworkCore.Model
+{
+    public partial class UserContext : DbContext
+    {
+        public UserContext()
+        {
+        }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options) { }        
+        public DbSet<User> Users => Set<User>();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Подключение к SQL Server из App.config
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["ConnectionLocalDb"].ToString());
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
+
+
+	
